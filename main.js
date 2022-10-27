ae3fd0b83b45a37a8239189867a861abd26f0efc
Davi Santos <davirr1220@gmail.com>
2022-10-27 17:38:50 -0300

    finalizando o projeto

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 
 const jump = () => {
@@ -8,5 +9,31 @@ const jump = () => {
         mario.classList.remove('jump');
     }, 500); 
 }
+
+     const loop = setInterval(() =>{
+
+        console.log('loop')
+
+     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+     console.log(marioPosition); 
+
+        const pipePosition = pipe.offsetLeft;
+         if (pipePosition <=  120 && pipePosition > 0&&marioPosition < 80) {
+
+           pipe.style.animation = 'none';
+           pipe.style.Left =`${ pipePosition}px`;
+
+           pipe.style.animation = 'none';
+           mario.style.bottom =`${ pipePosition}px`;
+
+           mario.src ='Imagens/game-over.png';
+           mario.style.width = '75px'
+          
+           clearInterval(loop);
+
+         }
+
+    }, 10) ;                          
 
 document.addEventListener('keydown',jump);
