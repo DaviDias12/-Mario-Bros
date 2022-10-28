88be329bc54a7a42beeb3bac63d580e5eac6a7e1
Davi Santos <davirr1220@gmail.com>
2022-10-28 12:32:44 -0300

    ultimo commit

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const textStart = document.querySelector('text-start');
 
 
 const jump = () => {
+
+    document.getElementById("text-start").style.color = "rgb(236, 236, 236)";
+
+
     mario.classList.add('jump');
 
     setTimeout(() => {
@@ -21,6 +26,11 @@ const jump = () => {
         const pipePosition = pipe.offsetLeft;
          if (pipePosition <=  120 && pipePosition > 0&&marioPosition < 80) {
 
+            
+
+            document.getElementById("text-start").innerHTML="<strong>GAME OVER</strong>";
+            document.getElementById("text-start").style.color = "black";
+
            pipe.style.animation = 'none';
            pipe.style.Left =`${ pipePosition}px`;
 
@@ -31,9 +41,6 @@ const jump = () => {
            mario.style.width = '75px'
           
            clearInterval(loop);
+         } }, 10) ;                          
 
-         }
-
-    }, 10) ;                          
-
-document.addEventListener('keydown',jump);
+document.addEventListener('keydown', jump);
